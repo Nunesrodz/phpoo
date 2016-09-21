637b963508e326db3c9810d1a68aed29dfb15560
Nunesrodz <rodrigo.o.nunes@gmail.com>
2016-09-21 20:03:08 -0300

    Aula 3 - Métodos, desconto em produto

--- class/Produto.php
+++ class/Produto.php
@@ -9,6 +9,14 @@ class Produto {
     public $categoria;
     public $usado;
 
+    public function precoComDesconto($valor = 0.1) {
+    	//negativo e limite de desconto
+    	// if ($valor > 0 && $valor <= 0.5) {
+	    //     $this->preco -= $this->preco * $valor;
+	    // }
+	    return $this->preco - ($this->preco * $valor);
+    }
+
 }
 
 ?>
